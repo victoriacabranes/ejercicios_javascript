1f6a5b5c7032a4e7385f6fef3fe5a9b834c4b387
victoriacabranes <victoriacabranesph@gmail.com>
2022-08-22 18:36:23 -0300

    correcciones en loop

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let adivina = Number(prompt("Adivina un numero del 1 al 4, tienes 3 intentos"));
-for(let i = 0; i<4; i++); {
-    
+
+for(let i = 0; i<3; i++) {
+    let adivina = Number(prompt("Adivina un numero del 1 al 4, tienes 3 intentos"));
     if (adivina === 3) {
         alert("Adivinaste! Bien ahi genio")
+        break;
     } else {
         alert("vuelve a intentarlo!")
     }
     
 }
-
 
 
 
