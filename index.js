f8ef57b4c8899786f2e7079d752698562f5804d7
victoriacabranes <victoriacabranesph@gmail.com>
2022-08-22 17:30:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let adivina = Number(prompt("Adivina un numero del 1 al 4, tienes 3 intentos"));
+for(let i = 0; i<4; i++); {
+    
+    if (adivina === 3) {
+        alert("Adivinaste! Bien ahi genio")
+    } else {
+        alert("vuelve a intentarlo!")
+    }
+    
+}
+
+
+
+
+
